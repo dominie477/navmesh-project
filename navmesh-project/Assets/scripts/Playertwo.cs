c5cc3809b9a5bc6eb82b9607d5e601e479970d8f
osher volotker <volotkero@gmail.com>
2021-07-22 21:54:09 +0300

    second upload
    fixed player 1 teleport, player 2 collider flxed, pressure pad area fixed, added new area and more enemies

---
+++ navmesh-project/Assets/scripts/Playertwo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyplayer : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10f;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        controller.Move(Physics.gravity * Time.deltaTime);
+
+
+    }
+}
